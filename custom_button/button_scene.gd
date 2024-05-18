80a64745d522a0ac6ea86a2c833ff9e9fd66187e
Michael <muxaujish@gmail.com>
2024-05-18 12:15:16 +0300

    added centercontainer

--- custom_button/button_scene.gd
+++ custom_button/button_scene.gd
@@ -1,17 +1,26 @@
-extends Node2D
+extends VBoxContainer
 
-const grid_cell_count = 5
+const grid_cell_count = 13
+const grid_cell_size = 32
+var mines_count = 10
 var grid_size = Vector2(grid_cell_count, grid_cell_count)
-var cell_size = Vector2(64,64)
+var cell_size = Vector2(grid_cell_size, grid_cell_size)
 var stretch = TextureButton.STRETCH_SCALE
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	create_label()
 	fill_grid_container()
 	pass # Replace with function body.
 
+func create_label():
+	var label = Label.new()
+	label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	label.text = "Mines left: %d" % [mines_count]
+	add_child(label)
+
 func fill_grid_container():
 	var grid_container = GridContainer.new()
-	grid_container.columns = 5
+	grid_container.columns = grid_cell_count
 	add_child(grid_container)
 	for y in range(grid_size.y):
 		for x in range(grid_size.x):
--- custom_button/custom_button.gd
+++ custom_button/custom_button.gd
@@ -2,8 +2,8 @@ extends TextureButton
 
 class_name SquareButton
 
-var texture_white = preload("res://custom_button/white_button.png")
-var texture_grey = preload("res://custom_button/grey_button.png")
+var texture_white = preload("res://custom_button/white_25.png")
+var texture_grey = preload("res://custom_button/grey_25.png")
 var stretch = TextureButton.STRETCH_SCALE
 var default_size = Vector2(32,32)
 
