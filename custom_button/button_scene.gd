b75b0f30268b69ac68fe3e5c2aa3f78b2f84bea0
Michael <muxaujish@gmail.com>
2024-05-18 13:22:16 +0300

    added pressed signal with lambda pass x,y

--- custom_button/button_scene.gd
+++ custom_button/button_scene.gd
@@ -3,6 +3,7 @@ extends VBoxContainer
 const grid_cell_count = 13
 const grid_cell_size = 32
 var mines_count = 10
+var mines_field = []
 var grid_size = Vector2(grid_cell_count, grid_cell_count)
 var cell_size = Vector2(grid_cell_size, grid_cell_size)
 var stretch = TextureButton.STRETCH_SCALE
@@ -23,9 +24,18 @@ func fill_grid_container():
 	grid_container.columns = grid_cell_count
 	add_child(grid_container)
 	for y in range(grid_size.y):
+		mines_field.append([])
 		for x in range(grid_size.x):
-			var cell_button = SquareButton.new(cell_size, stretch)
+			var cell_button = SquareButton.new(cell_size, stretch, x, y)
 			grid_container.add_child(cell_button)
+			# привязка сигнала к лямбда-функции с передачей параметров
+			cell_button.connect("pressed", func(): _on_button_pressed(x,y))
+			mines_field[y].append(cell_button)
+
+func _on_button_pressed(x,y):
+	#print("Button %d %d" % [x,y])
+	var cell_button = mines_field[y][x]
+	cell_button.disabled = true
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- custom_button/custom_button.gd
+++ custom_button/custom_button.gd
@@ -6,12 +6,18 @@ var texture_white = preload("res://custom_button/white_25.png")
 var texture_grey = preload("res://custom_button/grey_25.png")
 var stretch = TextureButton.STRETCH_SCALE
 var default_size = Vector2(32,32)
+var x: int = 0
+var y: int = 0
 
-func _init(default_size, stretch):
+func _init(default_size, stretch, x, y):
 	custom_minimum_size = default_size
 	stretch_mode = stretch
+	self.x = x
+	self.y = y
+	#print("x = %d, y = %d" % [x,y])
 	
 func _ready():
 	texture_normal = texture_white
 	texture_pressed = texture_grey
+	texture_disabled = texture_grey
 	
